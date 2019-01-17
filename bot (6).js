94207acb04d589741b7f99aa22e44aa6113d0005
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-17 13:05:27 +0200

    Add files via upload

---
+++ bot (6).js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+const developers = ["466012765090217994"]
+const adminprefix = "-";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Status You  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/ledom999");
+      message.channel.send(`**Status You ${argresult} **`)
+}
+});
+
+
+
+
+client.login("NDY2MDEyNzY1MDkwMjE3OTk0.DyHalA.UWNS_VH8tINwuwpUuBszgscidqg");
